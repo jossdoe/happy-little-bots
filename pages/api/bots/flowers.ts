1ec805934a6e39a7ad274eaae41da7dc1b863e08
Joss Doebler <mail@joss-doebler.de>
2021-10-30 19:45:09 +0200

    test flowerbed-multirow-string

--- pages/api/bots/flowers.ts
+++ pages/api/bots/flowers.ts
@@ -1,7 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+function buildFlowerBed() {
+  return `
+🌸🌸🌸🌱🌱🌱🌱🌱
+🌸🌸🌸🌱🌱🌱🌱🌱
+🌸🌸🌸🌱🌱🌱🌱🌱
+🌱🌱🌱🌱🌱🌱🌸🌸
+🌱🌱🌱🌱🌱🌱🌸🌸
+🌱🌱🌱🌱🌱🌱🌱🌱
+`
+}
+
 async function flowerBot(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ tweet: '🌸🌸🌸🌸🌸🌸' })
+  res.status(200).json({ tweet: buildFlowerBed() })
 }
 
 export default flowerBot
