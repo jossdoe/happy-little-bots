8faecc80973151ff6abda543fd934e479ab14d12
Joss Doebler <mail@joss-doebler.de>
2021-10-30 19:41:33 +0200

    clean-up

--- pages/api/bots/flowers.ts
+++ pages/api/bots/flowers.ts
@@ -1,11 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 async function flowerBot(_req: NextApiRequest, res: NextApiResponse) {
-  // const zRes = await fetch(process.env.URL_FLOWERS, {
-  //   method: 'POST',
-  //   body: JSON.stringify({ tweet: '🌸🌸🌸' }),
-  // })
-
   res.status(200).json({ tweet: '🌸🌸🌸🌸🌸🌸' })
 }
 
